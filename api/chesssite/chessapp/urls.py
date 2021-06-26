c613b9987adee640841bab8dab5a777ca4b77a5c
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-06-26 10:10:19 -0600

    finished rest framework tutorial

--- api/chesssite/chessapp/urls.py
+++ api/chesssite/chessapp/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
+from rest_framework.routers import DefaultRouter
 
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'games', views.GameViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
-    # path("", views.index, name="index"),
-    # path("creategame", views.createGame, name="creategame"),
-    path("", views.api_root),
-    path("games/", views.GameList.as_view()),
-    path("games/<int:pk>/", views.GameDetail.as_view()),
-    path("users/", views.UserList.as_view()),
-    path("users/<int:pk>/", views.UserDetail.as_view()),
+    path('', include(router.urls)),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
 
 urlpatterns += [path("api-auth/", include("rest_framework.urls"))]
