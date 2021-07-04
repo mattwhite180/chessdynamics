a5b8413e078b9fe68bbda26b3ed506b0b15ac506
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-07-03 22:49:12 -0600

    minor changes

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 docker-compose down --remove-orphans && \
 docker-compose build && \
 docker-compose run api bash -c \
-"sleep 10 && python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py test $1 $2 $3" && \
+"sleep 10 && python3 manage.py makemigrations chessapp && python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py test $1 $2 $3" && \
 docker-compose down --remove-orphans
