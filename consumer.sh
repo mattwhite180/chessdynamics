2940336051867f7d601938945a2082e6699cdc80
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-07-15 07:54:51 -0600

    consumer.sh automatically spins up apiCount - 1 consumers

--- consumer.sh
+++ consumer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for (( i=1; i<=$1; i++ ))
+count=$(docker ps 2> /dev/null | grep api | wc | awk '{print $1}')
+for (( i=1; i<$count; i++ ))
 do
     echo "docker exec chessdynamics_api_$i python3 manage.py testapi &"
     docker exec chessdynamics_api_$i python3 manage.py testapi &
