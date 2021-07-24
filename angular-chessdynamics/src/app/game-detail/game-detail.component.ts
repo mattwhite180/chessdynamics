1017f42316d1d59676cad1cf6bda64cfd225aabf
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-07-23 18:30:14 -0600

    buttons grey out automatically

--- angular-chessdynamics/src/app/game-detail/game-detail.component.ts
+++ angular-chessdynamics/src/app/game-detail/game-detail.component.ts
@@ -8,10 +8,9 @@ import { GameService } from '../game.service';
 @Component({
   selector: 'app-game-detail',
   templateUrl: './game-detail.component.html',
-  styleUrls: ['./game-detail.component.css']
+  styleUrls: ['./game-detail.component.css'],
 })
 export class GameDetailComponent implements OnInit {
-
   game: Game | undefined;
   legal_moves: any;
 
@@ -28,25 +27,25 @@ export class GameDetailComponent implements OnInit {
   refreshBoard(): void {
     this.getGame();
   }
-  
+
   getGame(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.gameService.getGame(id)
-      .subscribe(game => this.game = game);
+    this.gameService.getGame(id).subscribe((game) => (this.game = game));
   }
 
   save(): void {
     if (this.game) {
-      this.gameService.updateGame(this.game)
-        .subscribe(() => this.goBack());
+      this.gameService.updateGame(this.game).subscribe(() => this.goBack());
     }
+    this.getGame();
   }
 
   playTurn(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.gameService.apiAction(id, "play_turn").subscribe();
+    this.gameService.apiAction(id, 'play_turn').subscribe();
+    sleep(500);
+    this.getGame();
   }
-
 
   delete(game: Game): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
@@ -55,16 +54,29 @@ export class GameDetailComponent implements OnInit {
 
   pop(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.gameService.apiAction(id, "pop").subscribe();
+    this.gameService.apiAction(id, 'pop').subscribe();
+    sleep(500);
+    this.getGame();
   }
 
   playMove(move: String): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.gameService.apiAction(id, "play_move/" + move).subscribe();
+    this.gameService.apiAction(id, 'play_move/' + move).subscribe();
+    sleep(500);
+    this.getGame();
   }
 
   goBack(): void {
     this.location.back();
   }
+}
 
+async function init() {
+  await sleep(1000);
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
